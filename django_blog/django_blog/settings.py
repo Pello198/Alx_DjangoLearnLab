491ad80a3b12657e88802c32d49662d1b7b1a37c
pello <iankwemoi6@gmail.com>
2025-12-07 14:37:42 +0300

    changed settings file

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -79,7 +79,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'USER': '',      # added
+        'PASSWORD': '',  
+        'HOST': '',      
+        'PORT': '',
+           }
 }
 
 
